56a557f0cb8f2e51a5aff67d3ad4bd3323f38b78
josevictormoreno <josevictor.admoreno@gmail.com>
2023-02-14 21:49:21 -0300

    comando where

---
+++ comandos-basicos/where.sql
@@ -0,0 +1,6 @@
+-- COMANDO WHERE
+
+SELECT first_name from sales.customers where score>400 -- busca clientes com score maior que 400
+SELECT first_name from sales.customers where customers_id>10 -- busca clientes com id maior que 10
+SELECT model from sales.products where brand='FIAT' -- busca por modelos da marca fiat
+SELECT model from salec.products where price<50000 -- busca por modelos com preço menor que 50 mil
